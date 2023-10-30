1d13c99f8bd7414aac14879b1569d9224ab089e7
00011581 <wiut00011581@gmail.com>
2023-10-30 21:52:25 +0500

    Created Db models

---
+++ Models/Entities/Article.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models.Entities
+{
+    public class Article
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set;}
+
+        [Required]
+        public string Body { get; set;}
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]   // default value DateTime.Now
+        public DateTime CreatedAt { get; set; } 
+
+        [Required]
+        [ForeignKey("Topic")]
+        public int TopicId { get; set; }
+        public Topic Topic { get; set; }
+    }
+}
---
+++ Models/Entities/Topic.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Linq;
+
+namespace API.Models.Entities
+{
+    public class Topic
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Name { get; set; }
+        
+        [StringLength(255)]
+        public string? Description { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]   // default value DateTime.Now
+        public DateTime CreatedAt { get; set; }
+    }
+}
