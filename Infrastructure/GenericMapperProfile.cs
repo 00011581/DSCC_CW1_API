28e77a125a86155f3aecc0b69f4c78fa647cd9d6
00011581 <wiut00011581@gmail.com>
2023-10-31 10:39:59 +0500

    created AutoMapper

---
+++ Infrastructure/GenericMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using API.Models.DTOs;
+using API.Models.Entities;
+using AutoMapper;
+
+namespace API.Infrastructure
+{
+    public class GenericMapperProfile : Profile
+    {
+        public GenericMapperProfile()
+        {
+            CreateMap<Article, ArticleDTO>().ReverseMap();
+            CreateMap<Article, ArticleCreateDTO>().ReverseMap();
+            CreateMap<Topic, TopicDTO>().ReverseMap();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using API.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,9 @@ builder.Services.AddSwaggerGen();
 // DbContext
 var connectionString = builder.Configuration.GetConnectionString("LocalDb");
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
+// Mapping
+builder.Services.AddAutoMapper(typeof(GenericMapperProfile));
 
 var app = builder.Build();
 
