ebd5981a8d02144af62468ca66f4d5ecd50ef787
00011581 <wiut00011581@gmail.com>
2023-10-31 10:21:13 +0500

    created DTOs in Models/

---
+++ Models/DTOs/ArticleDTO.cs
@@ -0,0 +1,29 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models.DTOs
+{
+    public class ArticleDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int TopicId { get; set; }
+        public TopicDTO Topic { get; set; }
+    }
+
+    public class ArticleCreateDTO
+    {
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Body { get; set; }
+
+        [Required]
+        public int TopicId { get; set; }
+    }
+}
---
+++ Models/DTOs/TopicDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models.DTOs
+{
+    public class TopicDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
