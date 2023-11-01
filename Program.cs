f3edafd5c837d9380a5949220a0f7697954d1997
00011581 <wiut00011581@gmail.com>
2023-11-02 02:10:00 +0500

    enabled Swagger for production

--- Program.cs
+++ Program.cs
@@ -26,11 +26,14 @@ builder.Services.AddScoped<ITopicRepository, TopicRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
